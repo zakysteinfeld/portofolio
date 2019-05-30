9ac0ab2edbe008f801e7c796f06ce871ec825dd3
zakysteinfeld <51196879+zakysteinfeld@users.noreply.github.com>
2019-05-31 00:36:29 +0700

    fix about-me

--- src/module/about-me.js
+++ src/module/about-me.js
@@ -13,7 +13,7 @@ export default class AboutMe extends Component {
                         <Col>
                             <div className='card shadow' style={{ textAlign: 'justify' }}>
                                 <text>&nbsp;&nbsp;&nbsp;&nbsp;Hello there, my name is Muhamad Zaky, I am a Front-end Developer, and previously was a .NET Developer. 
-                                    I work for PT Walden Global Services Head Office in Bandung, Indonesia currently. Coding has been my job since 2 years ago. 
+                                    I work on PT Walden Global Services Head Office in Bandung, Indonesia currently. Coding has been my job since 2 years ago. 
                                     I am a graduate of SMK Negeri 4 Bandung majoring in Software Engineering.</text>
                             </div>
                         </Col>
