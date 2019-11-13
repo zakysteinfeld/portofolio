0b4d8a616b2a669761243fa56ee3f25dc61734bf
白山ざき <zakysteinfeld@outlook.com>
2019-11-14 01:10:28 +0700

    update 3.5.25

--- src/app/layout/footer.js
+++ src/app/layout/footer.js
@@ -16,12 +16,15 @@ export default class Footer extends Component {
           <Col span={12}>
             <span>&copy; 2019 ・ Developed with <Icon type='heart' theme='filled' /> by Muhamad Zaky</span>
           </Col>
-          {/* <Col span={12} className='pull-right'>
-            <Select defaultValue='en' onChange={this.handleChange}>
+          <Col span={12} className='pull-right'>
+            {/* <Select defaultValue='en' onChange={this.handleChange}>
               <Option value='en'>English</Option>
               <Option value='ja'>Japanese</Option>
-            </Select>
-          </Col> */}
+            </Select> */}
+            <a className='socmed' href='https://twitter.com/zakysteinfeld' target='_blank'><Icon type='twitter' /></a>
+            <a className='socmed' href='https://instagram.com/zakysteinfeld' target='_blank'><Icon type='instagram' theme='filled' /></a>
+            <a className='socmed' href='https://www.linkedin.com/in/muhamadzaky/' target='_blank'><Icon type='linkedin' theme='filled' /></a>
+          </Col>
         </Row>
       </div>
     )
